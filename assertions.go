2c0aa2f69fd8696e894ab622200c5a2508be664b
Niklas116 <Niklas.Keitsch@gmx.de>
2024-10-24 19:01:08 +0200

    msg

--- assertions.go
+++ assertions.go
@@ -1,84 +1,96 @@
 package assertions
 
 import (
+	"runtime"
 	"testing"
 )
 
-func True(t *testing.T, args ...bool) {
+var instance *testing.T
+
+func InitInstance(t *testing.T) {
+	instance = t
+}
+
+func True(args ...bool) {
 	for i := range args {
 		if !args[i] {
-			t.Logf("expected %v (%T) to be true", args[i], args[i])
-			t.Fail()
+			pc, file, line, ok := runtime.Caller(0)
+			if ok {
+				f := runtime.FuncForPC(pc)
+				instance.Logf("called by %s at %s: %d", f.Name(), file, line)
+			}
+			instance.Logf("expected %v (%T) to be true", args[i], args[i])
+			instance.Fail()
 		}
 	}
 }
 
-func False(t *testing.T, args ...bool) {
+func False(args ...bool) {
 	for i := range args {
 		if args[i] {
-			t.Logf("expected %v (%T) to be false", args[i], args[i])
-			t.Fail()
+			instance.Logf("expected %v (%T) to be false", args[i], args[i])
+			instance.Fail()
 		}
 	}
 }
 
-func Equal[T comparable](t *testing.T, args ...T) {
+func Equal[T comparable](args ...T) {
 	if len(args) <= 1 {
 		return
 	}
 	for i := range args[:len(args)-1] {
 		if args[i] != args[i+1] {
-			t.Logf("expected %v (%T) to equal %v (%T)", args[i], args[i], args[i+1], args[i+1])
-			t.Fail()
+			instance.Logf("expected %v (%T) to equal %v (%T)", args[i], args[i], args[i+1], args[i+1])
+			instance.Fail()
 		}
 	}
 }
 
-func Unequal[T comparable](t *testing.T, args ...T) {
+func Unequal[T comparable](args ...T) {
 	if len(args) <= 1 {
 		return
 	}
 	for i := range args[:len(args)-1] {
 		if args[i] == args[i+1] {
-			t.Log("expected", args[i], "to unequal", args[i+1])
-			t.Fail()
+			instance.Log("expected", args[i], "to unequal", args[i+1])
+			instance.Fail()
 		}
 	}
 }
 
-func NilPtr[T any](t *testing.T, args ...*T) {
+func NilPtr[T any](args ...*T) {
 	for _, arg := range args {
 		if arg != nil {
-			t.Log("expected", arg, "to equal nil")
-			t.Fail()
+			instance.Log("expected", arg, "to equal nil")
+			instance.Fail()
 		}
 	}
 }
 
-func NotNilPtr[T any](t *testing.T, args ...*T) {
+func NotNilPtr[T any](args ...*T) {
 	for _, arg := range args {
 		if arg == nil {
-			t.Log("expected", arg, "to not equal nil")
-			t.Fail()
+			instance.Log("expected", arg, "to not equal nil")
+			instance.Fail()
 		}
 	}
 }
 
-func PanicError(t *testing.T, f func(), e string) {
+func PanicError(f func(), e string) {
 	defer func() {
 		if r := recover(); r != nil {
 			if err, ok := r.(error); ok {
 				if err.Error() != e {
-					t.Log("function panicked with different error message")
-					t.Fail()
+					instance.Log("function panicked with different error message")
+					instance.Fail()
 				}
 			} else {
-				t.Log("function panicked without error")
-				t.Fail()
+				instance.Log("function panicked without error")
+				instance.Fail()
 			}
 		} else {
-			t.Log("function did not panic or no error was passed")
-			t.Fail()
+			instance.Log("function did not panic or no error was passed")
+			instance.Fail()
 		}
 	}()
 	f()
